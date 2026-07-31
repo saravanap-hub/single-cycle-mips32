024b5c7f2320e7edc18dcf310b5cad1c055cea01
saravana <saravanap107@gmail.com>
2026-07-31 23:49:08 +0530

    added instruction and data memory

---
+++ rtl/memory/dmem.v
@@ -0,0 +1,13 @@
+module dmem(
+    input         clk, we,
+    input  [31:0] a,
+    input  [31:0] wd,
+    output [31:0] rd
+);
+    reg [31:0] RAM[63:0];
+ 
+    assign rd = RAM[a[31:2]];
+ 
+    always @(posedge clk)
+        if (we) RAM[a[31:2]] <= wd;
+endmodule
